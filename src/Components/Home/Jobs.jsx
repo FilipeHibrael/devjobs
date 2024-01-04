2383091ab30444e17a518f7d3279955ac9e46f7e
FilipeHibrael <filipehibrael27@gmail.com>
2024-01-03 22:41:14 -0300

    useFetch

--- src/Components/Home/Jobs.jsx
+++ src/Components/Home/Jobs.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import dataJson from '../../data.json';
 import JobItem from './JobItem';
 import styles from './Jobs.module.css';
+import useFetch from '../../Hooks/useFetch';
 
 const Jobs = () => {
-  console.log(dataJson);
-  return (
-    <div className={styles.jobs}>
-      {dataJson.map((data) => (
-        <JobItem key={data.id} {...data} />
-      ))}
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    request('../../src/data.json');
+  }, [request]);
+
+  if (error) return <div>{error}</div>;
+  if (loading) return <div>Carregando...</div>;
+  if (data)
+    return (
+      <div className={styles.jobs}>
+        {dataJson.map((data) => (
+          <JobItem key={data.id} {...data} />
+        ))}
+      </div>
+    );
+  else return null;
 };
 
 export default Jobs;
