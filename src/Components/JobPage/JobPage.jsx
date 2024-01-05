31d955b73e6f3f8179e0462196f292b9bb90ec0f
FilipeHibrael <filipehibrael27@gmail.com>
2024-01-05 00:58:55 -0300

    job-page-header

--- src/Components/JobPage/JobPage.jsx
+++ src/Components/JobPage/JobPage.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import useFetch from '../../Hooks/useFetch';
 import Loading from '../Helper/Loading';
 import Error from '../Helper/Error';
+import JobPageHeader from './JobPageHeader';
+import styles from './JobPage.module.css'
 
 const JobPage = () => {
   const { id } = useParams();
@@ -19,7 +21,18 @@ const JobPage = () => {
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
-  if (data) return <div>{jobData ? jobData.position : 'Not Found'}</div>;
+  if (data)
+    return (
+      <>
+        {jobData ? (
+          <section className={styles.jobPage}>
+            <JobPageHeader {...jobData} />
+          </section>
+        ) : (
+          'Not Found'
+        )}
+      </>
+    );
 };
 
 export default JobPage;
---
+++ src/Components/JobPage/JobPageHeader.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './JobPageHeader.module.css';
+
+const JobPageHeader = ({ company, logo, logoBackground, website }) => {
+  return (
+    <div className={styles.jobPageHeader}>
+      <div className={styles.logo} style={{ background: logoBackground }}>
+        <img src={'../../src' + logo} alt="" />
+      </div>
+      <div className={styles.info}>
+        <h2>{company}</h2>
+        <span>{company.toLowerCase().replace(' ', '')}.com</span>
+      </div>
+      <a
+        className={`${styles.link} lightButton`}
+        href={website}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Company Site
+      </a>
+    </div>
+  );
+};
+
+export default JobPageHeader;
